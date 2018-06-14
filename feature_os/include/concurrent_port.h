f2af31c7da2b7eb97ba441333a90c2f2e634d420
Zuodian Hu <huzuodian@gmail.com>
2018-06-14 01:20:03 -0500

    Added Windows mutex creation

--- feature_os/include/concurrent_port.h
+++ feature_os/include/concurrent_port.h
@@ -1,24 +1,25 @@
 #ifndef _THREAD_PORT_H_
 #define _THREAD_PORT_H_
 
-#if (__STDC_VERSION__ >= 201112L) && (!defined __STDC_NO_THREADS__) // C11 atomics
-	#define _CONCURRENT_C11_
-#else // not C11
-	// Choose WIN32 or POSIX
-	#ifdef WIN32
-		#define _CONCURRENT_WIN32_
-	#else
-		#define _CONCURRENT_POSIX_
-	#endif
-#endif // #if (__STDC_VERSION__ >= 201112L) && (!defined __STDC_NO_THREADS__)
+#ifdef _WIN32
 
-#ifdef _CONCURRENT_C11_
-	#include <stdatomic.h>
-	#include <threads.h>
-#else ifdef _CONCURRENT_WIN32_
-	#include <Windows.h>
-#else ifdef _CONCURRENT_POSIX_
-	#include <pthread.h>
+#include <Windows.h>
+
+#define thrpt_mutex HANDLE
+#define thrpt_mutex_init(mutex) \
+	do { \
+		mutex = CreateMutex(NULL, false, NULL); \
+	} while(0)
+
+#else
+
+#include <pthread.h>
+
+#define thrpt_mutex pthread_mutex_t
+
+#define thrpt_mutex_init(mutex) pthread_mutex_init(&mutex, NULL)
+#define thrpt_mutex_lock(mutex) pthread_mutex_lock(&mutex)
+
 #endif
 
 #endif // #ifndef _THREAD_PORT_H_
