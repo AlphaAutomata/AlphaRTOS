93ce2f78eab105632f0566b0ffb122d524ebe44a
Zuodian Hu <huzuodian@gmail.com>
2018-06-14 02:04:05 -0500

    Initial versions of mutex create, lock, unlock, and destroy macros for both Windows and POSIX.

--- feature_os/include/concurrent_port.h
+++ feature_os/include/concurrent_port.h
@@ -5,20 +5,26 @@
 
 #include <Windows.h>
 
-#define thrpt_mutex HANDLE
-#define thrpt_mutex_init(mutex) \
-	do { \
+#define concurr_mutex HANDLE
+
+#define concurr_mutex_init(mutex)               \
+	do {                                        \
 		mutex = CreateMutex(NULL, false, NULL); \
 	} while(0)
+#define concurr_mutex_lock(mutex)    WaitForSingleObject(mutex, INFINITE)
+#define concurr_mutex_unlock(mutex)  ReleaseMutex(mutex)
+#define concurr_mutex_destroy(mutex) CloseHandle(mutex)
 
 #else
 
 #include <pthread.h>
 
-#define thrpt_mutex pthread_mutex_t
+#define concurr_mutex pthread_mutex_t
 
-#define thrpt_mutex_init(mutex) pthread_mutex_init(&mutex, NULL)
-#define thrpt_mutex_lock(mutex) pthread_mutex_lock(&mutex)
+#define concurr_mutex_init(mutex)    pthread_mutex_init(&mutex, NULL)
+#define concurr_mutex_lock(mutex)    pthread_mutex_lock(&mutex)
+#define concurr_mutex_unlock(mutex)  pthread_mutex_unlock(&mutex)
+#define concurr_mutex_destroy(mutex) pthread_mutex_destroy(&mutex)
 
 #endif
 
