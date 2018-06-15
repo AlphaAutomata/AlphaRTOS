9a3f15daa742c8f1fb3d292e8f1c2836fa9fe987
Zuodian Hu <huzuodian@gmail.com>
2018-06-14 21:11:07 -0500

    Documented simple mutex type and functions. Ignoring diff and patch files.

--- feature_os/include/concurrent_port.h
+++ feature_os/include/concurrent_port.h
@@ -1,5 +1,97 @@
-#ifndef _THREAD_PORT_H_
-#define _THREAD_PORT_H_
+/**
+ * \defgroup CONCURRENT_PORT Cross-Platform Concurrency
+ *
+ * Utilities for concurrency and atomic operations.
+ *
+ * \{
+ */
+
+/**
+ * \file concurrent_port.h
+ *
+ * Cross-platform concurrency utilities.
+ *
+ * This header provides utilities for guarenteeing atomic operations.
+ */
+
+#ifndef _CONCURRENT_PORT_H_
+#define _CONCURRENT_PORT_H_
+
+/**
+ * \def concurr_mutex
+ *
+ * \hideinitializer
+ *
+ * Simple, non-recursive mutex.
+ *
+ * A ::concurr_mutex must be initialized by ::concurr_mutex_init() before it can be used. After
+ * initialization, it can be acquired by a single owner using ::concurr_mutex_lock(), and released
+ * using ::concurr_mutex_unlock(). When there is no owner, a ::concurr_mutex can be de-initialized
+ * using ::concurr_mutex_destroy().
+ */
+
+/**
+ * \def concurr_mutex_init
+ *
+ * \hideinitializer
+ *
+ * Initialize a ::concurr_mutex.
+ *
+ * All ::concurr_mutex variables must be initialized before use.
+ *
+ * \param mutex An uninitialized ::concurr_mutex.
+ *
+ * \retval None
+ */
+
+/**
+ * \def concurr_mutex_lock
+ *
+ * \hideinitializer
+ *
+ * Acquire ownership of an initialized ::concurr_mutex.
+ *
+ * If \a mutex is owned by another thread, the calling thread blocks until that ownership is
+ * relinquished. When the ownership is relinquished, it transfers to exactly one thread currently
+ * attempting to acquire ownership.
+ *
+ * \warning ::concurr_mutex \a mutex is not recursive. The thread that owns \a mutex trying to
+ *          acquire \a mutex again results in deadlock.
+ *
+ * \param mutex An initialized ::concurr_mutex to acquire ownership of.
+ *
+ * \retval None
+ *
+ * \return Returns only when the calling thread owns \a mutex.
+ */
+
+/**
+ * \def concurr_mutex_unlock
+ *
+ * \hideinitializer
+ *
+ * Relinquish ownership of a ::concurr_mutex this thread already owns.
+ *
+ * \warning Behavior is undefined if the calling thread does not own \a mutex.
+ *
+ * \param mutex A ::concurr_mutex this thread already owns.
+ *
+ * \retval None
+ */
+
+/**
+ * \def concurr_mutex_destroy
+ *
+ * \hideinitializer
+ *
+ * De-initialize an initialized and owner-less ::concurr_mutex.
+ *
+ * \warning Behavior is undefined if \a mutex is locked or uninitialized.
+ *
+ * \param mutex A ::concurr_mutex that's initialized but has no owner.
+ *
+ * \retval None
+ */
 
 #ifdef _WIN32
 
@@ -28,4 +120,8 @@
 
 #endif
 
-#endif // #ifndef _THREAD_PORT_H_
+#endif // #ifndef _CONCURRENT_PORT_H_
+
+/**
+ * \}
+ */
