4894eb4210f68ad9911ad83ac456cfb6f5bae225
Zuodian Hu <huzuodian@gmail.com>
2018-06-17 20:41:23 -0500

    Moved OS compatibility defines from ip-exact repo.

--- feature_os/include/feature_os_port.h
+++ feature_os/include/feature_os_port.h
@@ -1,4 +1,26 @@
+/**
+ * \file feature_os_port.h
+ *
+ * \todo Document this header
+ */
+
 #ifndef _FEATURE_OS_PORT_H_
 #define _FEATURE_OS_PORT_H_
+
+#ifdef WIN32
+
+#include <Windows.h>
+
+#include <direct.h>
+
+#define PATH_MAX MAX_PATH
+
+#else
+
+#include <unistd.h>
+
+#define MAX_PATH PATH_MAX
+
+#endif
 
 #endif
