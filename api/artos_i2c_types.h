62cb67e13d369b6b2bc426140a2362b2a3bc8198
Zuodian Hu <huzuodian@gmail.com>
2018-09-18 17:16:44 -0500

    Added more I2C event types.

--- api/artos_i2c_types.h
+++ api/artos_i2c_types.h
@@ -18,7 +18,9 @@ typedef enum ARTOS_eI2CEvent_ {
     ARTOS_eI2CEvent_BUS_ERR  = 0, //!< An I2C bus error.
     ARTOS_eI2CEvent_BUS_STOP = 1, //!< An I2C bus STOP event has been detected.
     ARTOS_eI2CEvent_TRX_DONE = 2, //!< An I2C bus transaction has finished.
-    ARTOS_eI2CEvent_NO_ACK   = 3  //!< No response to an I2C bus request.
+    ARTOS_eI2CEvent_NO_ACK   = 3, //!< No response to an I2C bus request.
+    ARTOS_eI2CEvent_REQ_RD   = 4, //!< Received read request from remote master.
+    ARTOS_eI2CEvent_REQ_WR   = 5  //!< Received write request from remote master.
 } ARTOS_eI2CEvent;
 
 /**
@@ -27,6 +29,7 @@ typedef enum ARTOS_eI2CEvent_ {
 typedef struct ARTOS_Event_I2C_ {
     ARTOS_eI2C      dev;      //!< The identifier of the device that triggered the event.
     ARTOS_eI2CEvent event;    //!< The type of event that occurred.
+    uint_fast16_t   addr;     //!< The address of the remote I2C device.
     size_t          data_len; //!< The length of the data buffer.
     uint8_t         data[];   //!< The buffer containing data transceived over I2C.
 } ARTOS_Event_I2C;
