66fdbc96d8ba17639f80a1fee0ecc029ef0e1432
Zuodian Hu <huzuodian@gmail.com>
2018-09-19 04:27:43 -0500

    Fixed function pointer language linkage for user event handlers.

--- api/artos_service_types.h
+++ api/artos_service_types.h
@@ -1,6 +1,10 @@
 #ifndef ARTOS_SERVICE_TYPES_H
 #define ARTOS_SERVICE_TYPES_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif // #ifdef __cplusplus
+
 /**
  * \brief Alpha RTOS generic handler.
  *
@@ -10,5 +14,9 @@
  * \retval ::ARTOS_eStatus_GENERIC_ERR Function did not exit properly.
  */
 typedef void (*pFn_serviceHandler)(void* arg);
+
+#ifdef __cplusplus
+}
+#endif // #ifdef __cplusplus
 
 #endif // #ifndef ARTOS_SERVICE_TYPES_H
