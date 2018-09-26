63dc3545584ee66ad70c4da66daea644aa71cc09
Zuodian Hu <huzuodian@gmail.com>
2018-09-26 18:50:25 -0500

    Updated scheduler to use new types and global tables. #3

--- kernel/src/scheduler.c
+++ kernel/src/scheduler.c
@@ -4,30 +4,57 @@
 
 #include "gp_timer_port.h"
 
+#include "core_ca.h"
+
 #include "task.h"
 
 #include "scheduler.h"
 
-volatile uint64_t uptime;
+static volatile uint64_t uptime;
 
-volatile bool runScheduler;
+static schedTable_t allTables[NUM_CORES];
+static regframe_t   kInstFrames[NUM_CORES];
+
+static inline regframe_t* getKernelContext(void) {
+	uint32_t coreID;
+	
+	coreID = __get_MPIDR();
+
+	return &(kInstFrames[coreID]);
+}
+
+static inline regframe_t* getMyContext(void) {
+	uint32_t coreID;
+	uint32_t mySP;
+	
+	coreID = __get_MPIDR();
+	mySP = __get_SP();
+
+	for (int i=0; i<NUM_UNITS; i++) {
+		intptr_t myStackBase;
+
+		myStackBase = (intptr_t)(allTables[coreID].units[i].attributes.stack_mem);
+
+		if (mySP >= myStackBase && mySP <= myStackBase+STACK_SIZE) {
+			return &(allTables[coreID].units[i].context);
+		}
+	}
+
+	return NULL;
+}
 
 /**
-//! Interrupt service routine to make sure no task overruns its allotted
-//! scheduling slot. Implemented in memoryS.s, since it forces a context switch
-//!
-//! \param none
-//!
-//! \return none
-//
+ * \brief Interrupt service routine to make sure no task overruns its allotted scheduling slot.
  */
-extern void TIMER0A_Handler(void);
+static void schedWatchdog_handler(void) {
+	regframe_t* kContext = getKernelContext();
+	regframe_t* myContext = getMyContext();
+	switchContext(kContext, myContext);
+}
 
-void initScheduler(taskTable_t* tasks) {
+void initScheduler(schedTable_t* table) {
 	uptime = 0;
 	
-	runScheduler = false;
-
 	// Set up a General Purpose Timer which will be used to ensure each task only runs for a limited
 	// amount of time. The time slots are set and the interrupts enabled at the beginning of each
 	// scheduling round, and the timer is disabled at the end of each scheduling round.
@@ -38,21 +65,16 @@ void initScheduler(taskTable_t* tasks) {
 		.rpt               = gpTimer_rpt_ONESHOT,
 		.start_immediately = false
 	};
-	gpTimer_init(gpTimer_inst_00, TIMER0A_Handler, &info);
+	gpTimer_init(gpTimer_inst_00, schedWatchdog_handler, &info);
 }
 
-void schedule(taskTable_t* tasks) {
+void schedule(schedTable_t* table) {
 	unsigned int i;
 	
-	if (tasks->currTaskID != 0) {
-		// Should never enter the scheduler when there is already a task running
-		while(1);
-	}
-
 	// Set timer so each task has the same amount of time scheduled for it. currTasks+1 sets
 	// aside time for OS operations.
 	gpTimer_info info = {
-		.loadValue         = SYSTICK_INTERVAL / (tasks->currNumTasks+1),
+		.loadValue         = SYSTICK_INTERVAL / NUM_UNITS,
 		.tripValue         = 0,
 		.cntDir            = gpTimer_cntDir_DOWN,
 		.rpt               = gpTimer_rpt_ONESHOT,
@@ -60,67 +82,21 @@ void schedule(taskTable_t* tasks) {
 	};
 	gpTimer_cfg_info(gpTimer_inst_00, &info);
 	
-	for (i=1; i<=tasks->currNumTasks; i++) {
+	for (i=1; i<=NUM_UNITS; i++) {
 		// Reload the timer at the start of each task.
 		gpTimer_arm(gpTimer_inst_00);
 		
 		// For each task, determine action based on its status
-		switch (tasks->tasks[i].status) {
-			case TASK_STATUS_UNINITIALIZED :
-				break;
-			case TASK_STATUS_RUNNING : // fallthrough
-			case TASK_STATUS_PREEMPTED :
-				// perform context switch and run the task
-				tasks->currTaskID = i;
-				tasks->tasks[i].frame.LR = (uint32_t)userReturn;
-				tasks->tasks[i].frame.SP = (uint32_t)(frameBase(i));
-				runTask(&(tasks->tasks[i].frame), &kframe, tasks->tasks[i].taskEntry);
-				tasks->currTaskID = 0;
-				break;
-			case TASK_STATUS_YIELDING :
-				// if the task had previously yielded the processor, let it run again
-				tasks->currTaskID = i;
-				tasks->tasks[i].status = TASK_STATUS_RUNNING;
-				switchContext(&(tasks->tasks[i].frame), &kframe);
-				tasks->currTaskID = 0;
-				break;
-			default :
-				break;
+		if (table->units[i].state == task_state_READY) {
+			table->units[i].state = task_state_RUNNING;
+			// perform context switch and run the task
+			table->units[i].context.LR = (uint32_t)userReturn;
+			table->units[i].context.SP = (uint32_t)(frameBase(i));
+			switchContext(getKernelContext(), &(table->units[i].context));
+			table->units[i].state = task_state_READY;
 		}
 	}
 	
 	// Disable the timer interrupt when done scheduling to avoid unnecessary CPU load from ISRs
-	gpTimer_disarm(gpTimer_inst_00);
-}
-
-void schedulePreempted(taskTable_t* tasks) {
-	int i;
-
-	// Set timer so each task has the same amount of time scheduled for it. currTasks+1 sets
-	// aside time for OS operations.
-	gpTimer_info info = {
-		.loadValue         = SYSTICK_INTERVAL / (tasks->currNumTasks+1),
-		.tripValue         = 0,
-		.cntDir            = gpTimer_cntDir_DOWN,
-		.rpt               = gpTimer_rpt_ONESHOT,
-		.start_immediately = false
-	};
-	gpTimer_cfg_info(gpTimer_inst_00, &info);
-	
-	for (i=1; i<=tasks->currNumTasks; i++) {
-		// Reload the timer at the start of each task.
-		gpTimer_arm(gpTimer_inst_00);
-		
-		// For each task, determine action based on its status
-		if (tasks->tasks[i].status == TASK_STATUS_PREEMPTED) {
-			// if the task had been preempted, let it run again
-			tasks->currTaskID = i;
-			tasks->tasks[i].status = TASK_STATUS_RUNNING;
-			switchContext(&(tasks->tasks[i].frame), &kframe);
-			tasks->currTaskID = 0;
-		}
-	}
-	
-	// disable the timer interrupt when done scheduling to avoid unnecessary CPU load from ISRs
 	gpTimer_disarm(gpTimer_inst_00);
 }
