00519eacf78f63c83067a523ac73f1b716947077
Zuodian Hu <huzuodian@gmail.com>
2018-12-02 19:10:36 -0600

    Refactor to consolidate common timer types and add a private timer.

--- baremetal/impl/src/hal_timer_gp.c
+++ baremetal/impl/src/hal_timer_gp.c
@@ -43,64 +43,64 @@ static XScuTimer xscu_timer_07;
 #endif
 // #if ((HAL_PLATFORM == HAL_PLATFORM_XSCU_Z7xxx ) || (HAL_PLATFORM == HAL_PLATFORM_XSCU_Z7xxxS))
 
-void hal_timerGp_init(hal_timerGp_inst instance, void* gpTimerHandler, hal_timerGp_info* info) {
+void hal_timerGp_init(hal_timer_inst_t inst, hal_timer_isr_t handler, hal_timerGp_info* info) {
 #if ((HAL_PLATFORM == HAL_PLATFORM_XSCU_Z7xxx) || (HAL_PLATFORM == HAL_PLATFORM_XSCU_Z7xxxS))
     u16 dev_id;
-    XScuTimer* timer_inst;
-    switch (instance) {
+    XScuTimer* x_timer_inst;
+    switch (inst) {
 #if (XPAR_XSCUTIMER_NUM_INSTANCES > 0)
-        case hal_timerGp_inst_00:
+        case hal_timer_inst_00:
             dev_id = XPAR_XSCUTIMER_0_DEVICE_ID;
-            timer_inst = &xscu_timer_00;
+            x_timer_inst = &xscu_timer_00;
             break;
 #endif // #if (XPAR_XSCUTIMER_NUM_INSTANCES > 0)
 
 #if (XPAR_XSCUTIMER_NUM_INSTANCES > 1)
-        case hal_timerGp_inst_01:
+        case hal_timer_inst_01:
             dev_id = XPAR_XSCUTIMER_1_DEVICE_ID;
-            timer_inst = &xscu_timer_01;
+            x_timer_inst = &xscu_timer_01;
             break;
 #endif // #if (XPAR_XSCUTIMER_NUM_INSTANCES > 1)
 
 #if (XPAR_XSCUTIMER_NUM_INSTANCES > 2)
-        case hal_timerGp_inst_02:
+        case hal_timer_inst_02:
             dev_id = XPAR_XSCUTIMER_2_DEVICE_ID;
-            timer_inst = &xscu_timer_02;
+            x_timer_inst = &xscu_timer_02;
             break;
 #endif // #if (XPAR_XSCUTIMER_NUM_INSTANCES > 2)
 
 #if (XPAR_XSCUTIMER_NUM_INSTANCES > 3)
-        case hal_timerGp_inst_03:
+        case hal_timer_inst_03:
             dev_id = XPAR_XSCUTIMER_3_DEVICE_ID;
-            timer_inst = &xscu_timer_03;
+            x_timer_inst = &xscu_timer_03;
             break;
 #endif // #if (XPAR_XSCUTIMER_NUM_INSTANCES > 3)
 
 #if (XPAR_XSCUTIMER_NUM_INSTANCES > 4)
-        case hal_timerGp_inst_04:
+        case hal_timer_inst_04:
             dev_id = XPAR_XSCUTIMER_4_DEVICE_ID;
-            timer_inst = &xscu_timer_04;
+            x_timer_inst = &xscu_timer_04;
             break;
 #endif // #if (XPAR_XSCUTIMER_NUM_INSTANCES > 4)
 
 #if (XPAR_XSCUTIMER_NUM_INSTANCES > 5)
-        case hal_timerGp_inst_05:
+        case hal_timer_inst_05:
             dev_id = XPAR_XSCUTIMER_5_DEVICE_ID;
-            timer_inst = &xscu_timer_05;
+            x_timer_inst = &xscu_timer_05;
             break;
 #endif // #if (XPAR_XSCUTIMER_NUM_INSTANCES > 5)
 
 #if (XPAR_XSCUTIMER_NUM_INSTANCES > 6)
-        case hal_timerGp_inst_06:
+        case hal_timer_inst_06:
             dev_id = XPAR_XSCUTIMER_6_DEVICE_ID;
-            timer_inst = &xscu_timer_06;
+            x_timer_inst = &xscu_timer_06;
             break;
 #endif // #if (XPAR_XSCUTIMER_NUM_INSTANCES > 6)
 
 #if (XPAR_XSCUTIMER_NUM_INSTANCES > 7)
-        case hal_timerGp_inst_07:
+        case hal_timer_inst_07:
             dev_id = XPAR_XSCUTIMER_7_DEVICE_ID;
-            timer_inst = &xscu_timer_07;
+            x_timer_inst = &xscu_timer_07;
             break;
 #endif // #if (XPAR_XSCUTIMER_NUM_INSTANCES > 7)
 
@@ -110,15 +110,15 @@ void hal_timerGp_init(hal_timerGp_inst instance, void* gpTimerHandler, hal_timer
 
     XScuTimer_Config* xscu_cfg = XScuTimer_LookupConfig(dev_id);
 
-    XScuTimer_CfgInitialize(timer_inst, xscu_cfg, xscu_cfg->BaseAddr);
-    XScuTimer_LoadTimer(timer_inst, info->loadValue);
+    XScuTimer_CfgInitialize(x_timer_inst, xscu_cfg, xscu_cfg->BaseAddr);
+    XScuTimer_LoadTimer(x_timer_inst, info->loadValue);
     switch (info->rpt) {
-        case hal_timerGp_rpt_ONESHOT:
-            XScuTimer_DisableAutoReload(timer_inst);
+        case hal_timer_rpt_ONESHOT:
+            XScuTimer_DisableAutoReload(x_timer_inst);
             break;
 
-        case hal_timerGp_rpt_REPEATED:
-            XScuTimer_EnableAutoReload(timer_inst);
+        case hal_timer_rpt_REPEATED:
+            XScuTimer_EnableAutoReload(x_timer_inst);
             break;
 
         default:
@@ -126,31 +126,31 @@ void hal_timerGp_init(hal_timerGp_inst instance, void* gpTimerHandler, hal_timer
     }
 
     IRQ_Enable(XPAR_SCUTIMER_INTR);
-    IRQ_SetHandler(XPAR_SCUTIMER_INTR, gpTimerHandler);
+    IRQ_SetHandler(XPAR_SCUTIMER_INTR, handler);
 
     if (info->start_immediately) {
-        XScuTimer_Start(timer_inst);
+        XScuTimer_Start(x_timer_inst);
     }
 #endif
 // #if ((HAL_PLATFORM == HAL_PLATFORM_XSCU_Z7xxx) || (HAL_PLATFORM == HAL_PLATFORM_XSCU_Z7xxxS))
 }
 
-void hal_timerGp_cfg(hal_timerGp_inst instance, void* gpTimerHandler, hal_timerGp_info* info) {
+void hal_timerGp_cfg(hal_timer_inst_t inst, hal_timer_isr_t handler, hal_timerGp_info* info) {
     return;
 }
 
-void hal_timerGp_cfg_handler(hal_timerGp_inst instance, void* gpTimerHandler) {
+void hal_timerGp_cfg_handler(hal_timer_inst_t inst, hal_timer_isr_t handler) {
     return;
 }
 
-void hal_timerGp_cfg_info(hal_timerGp_inst instance, hal_timerGp_info* info) {
+void hal_timerGp_cfg_info(hal_timer_inst_t inst, hal_timerGp_info* info) {
     return;
 }
 
-void hal_timerGp_arm(hal_timerGp_inst instance) {
+void hal_timerGp_arm(hal_timer_inst_t inst) {
     return;
 }
 
-void hal_timerGp_disarm(hal_timerGp_inst instance) {
+void hal_timerGp_disarm(hal_timer_inst_t inst) {
     return;
 }
