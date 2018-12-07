9a1001e0371709f3ac9bdc5777b5a2d56b06c03b
Zuodian Hu <huzuodian@gmail.com>
2018-12-06 18:52:30 -0600

    Checkpoint.

--- baremetal/api/hal_core.h
+++ baremetal/api/hal_core.h
@@ -52,25 +52,103 @@
 
 #if (HAL_PLATFORM == HAL_PLATFORM_XC7Zxxx)
 
-    #include "ARMCA9.h"
-	#include "mem_ARMCA9.h"
+	#define __CA_REV      (0x0300U)
+	#define __CORTEX_A    (     9U)
+	#define __FPU_PRESENT (     1U)
+	#define __GIC_PRESENT (     1U)
+	#define __TIM_PRESENT (     1U)
+	#define __L2C_PRESENT (     1U)
+
+	#define GIC_DISTRIBUTOR_BASE (0xF8F01000)
+	#define GIC_INTERFACE_BASE   (0xF8F00100)
+	#define TIMER_BASE           (0xF8F00600)
+
+	#define L2C_310_BASE         (0xF8F02000)
+
+	typedef enum IRQn
+	{
+		SGI0_IRQn            =  0, //!< Software Generated Interrupt 0
+		SGI1_IRQn            =  1, //!< Software Generated Interrupt 1
+		SGI2_IRQn            =  2, //!< Software Generated Interrupt 2
+		SGI3_IRQn            =  3, //!< Software Generated Interrupt 3
+		SGI4_IRQn            =  4, //!< Software Generated Interrupt 4
+		SGI5_IRQn            =  5, //!< Software Generated Interrupt 5
+		SGI6_IRQn            =  6, //!< Software Generated Interrupt 6
+		SGI7_IRQn            =  7, //!< Software Generated Interrupt 7
+		SGI8_IRQn            =  8, //!< Software Generated Interrupt 8
+		SGI9_IRQn            =  9, //!< Software Generated Interrupt 9
+		SGI10_IRQn           = 10, //!< Software Generated Interrupt 10
+		SGI11_IRQn           = 11, //!< Software Generated Interrupt 11
+		SGI12_IRQn           = 12, //!< Software Generated Interrupt 12
+		SGI13_IRQn           = 13, //!< Software Generated Interrupt 13
+		SGI14_IRQn           = 14, //!< Software Generated Interrupt 14
+		SGI15_IRQn           = 15, //!< Software Generated Interrupt 15
+
+		GlobalTimer_IRQn     = 27, //!< Global Timer Interrupt
+		PrivTimer_IRQn       = 29, //!< Private Timer Interrupt
+		PrivWatchdog_IRQn    = 30  //!< Private Watchdog Interrupt
+	} IRQn_Type;
+
+    #include "core_ca.h"
 
     #define HAL_ARCH       (HAL_ARCH_ARMCA9)
     #define HAL_NUM_CORES  (2)
 
-    #define HAL_STACK_SIZE (__STACK_SIZE)
-    #define HAL_STACK_BASE (__RAM_BASE+__RAM_SIZE)
+    #define HAL_STACK_SIZE ((&hal_ram_end) - (&hal_ram_base))
+    #define HAL_STACK_BASE (&hal_ram_base)
+
+	extern int hal_ram_base;
+	extern int hal_ram_end;
 
 #elif (HAL_PLATFORM == HAL_PLATFORM_XC7ZxxxS) // #if (HAL_PLATFORM == HAL_PLATFORM_XC7Zxxx)
 
-    #include "ARMCA9.h"
-	#include "mem_ARMCA9.h"
+	#define __CA_REV      (0x0300U)
+	#define __CORTEX_A    (     9U)
+	#define __FPU_PRESENT (     1U)
+	#define __GIC_PRESENT (     1U)
+	#define __TIM_PRESENT (     1U)
+	#define __L2C_PRESENT (     1U)
+
+	#define GIC_DISTRIBUTOR_BASE (0xF8F01000)
+	#define GIC_INTERFACE_BASE   (0xF8F00100)
+	#define TIMER_BASE           (0xF8F00600)
+
+	#define L2C_310_BASE         (0xF8F02000)
+
+	typedef enum IRQn
+	{
+		SGI0_IRQn            =  0, //!< Software Generated Interrupt 0
+		SGI1_IRQn            =  1, //!< Software Generated Interrupt 1
+		SGI2_IRQn            =  2, //!< Software Generated Interrupt 2
+		SGI3_IRQn            =  3, //!< Software Generated Interrupt 3
+		SGI4_IRQn            =  4, //!< Software Generated Interrupt 4
+		SGI5_IRQn            =  5, //!< Software Generated Interrupt 5
+		SGI6_IRQn            =  6, //!< Software Generated Interrupt 6
+		SGI7_IRQn            =  7, //!< Software Generated Interrupt 7
+		SGI8_IRQn            =  8, //!< Software Generated Interrupt 8
+		SGI9_IRQn            =  9, //!< Software Generated Interrupt 9
+		SGI10_IRQn           = 10, //!< Software Generated Interrupt 10
+		SGI11_IRQn           = 11, //!< Software Generated Interrupt 11
+		SGI12_IRQn           = 12, //!< Software Generated Interrupt 12
+		SGI13_IRQn           = 13, //!< Software Generated Interrupt 13
+		SGI14_IRQn           = 14, //!< Software Generated Interrupt 14
+		SGI15_IRQn           = 15, //!< Software Generated Interrupt 15
+
+		GlobalTimer_IRQn     = 27, //!< Global Timer Interrupt
+		PrivTimer_IRQn       = 29, //!< Private Timer Interrupt
+		PrivWatchdog_IRQn    = 30  //!< Private Watchdog Interrupt
+	} IRQn_Type;
+
+    #include "core_ca.h"
 
     #define HAL_ARCH       (HAL_ARCH_ARMCA9)
     #define HAL_NUM_CORES  (1)
 
-    #define HAL_STACK_SIZE (__STACK_SIZE)
-    #define HAL_STACK_BASE (__RAM_BASE+__RAM_SIZE)
+    #define HAL_STACK_SIZE ((&hal_ram_end) - (&hal_ram_base))
+    #define HAL_STACK_BASE (&hal_ram_base)
+
+	extern int hal_ram_base;
+	extern int hal_ram_end;
 
 #else // #if (HAL_PLATFORM == HAL_PLATFORM_XC7Zxxx)
       // ...
